5071d3f58ac3ab9e62eb3118efee82fdc411e551
Diego Rodríguez Martín <91576069+DiegoRM22@users.noreply.github.com>
2024-02-22 13:24:58 +0000

    g

--- clasification.py
+++ clasification.py
@@ -1,1 +1,15 @@
 
+
+# Instalamos el paquetes corrplot que contiene funciones para visualizar la matriz de correlación.
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('homeLoanAproval.csv')
+
+# Visualizamos las primeras 5 filas del dataset
+plt.scatter(data['ApplicantIncome'], data['LoanAmount'], marker='o')
+plt.title('ApplicantIncome vs LoanAmount')
+plt.xlabel('ApplicantIncome')
+plt.ylabel('LoanAmount')
+
+plt.show()
